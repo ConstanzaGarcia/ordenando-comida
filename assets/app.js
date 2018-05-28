3d480c933ec15d52152cee456ababd253d64b31d
ConstanzaGarcia <o.conii.o@gmail.com>
2018-05-28 19:39:05 -0400

    Corrigiendo JS

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,13 @@
-window.onload = function () {
+const menu = function (request) {
+  if (Ceviche < 5) {
+    return 'Tu plato estará listo en 30 minutos';
+  }
+  else if (Tacos < 4) {
+    return 'En 15 minutos tu plato estara lista';
+  }
+  else if (EmpanadaChilena < 0 ) {
+  return 'No tenemos en estos momentos pero hay mas opciones para elegir';
 }
+ else if (Hambuerguesas < 0) {
+  return "Nos encantan las hamburguesas,pero se nos acabaron"; 
+; } }
