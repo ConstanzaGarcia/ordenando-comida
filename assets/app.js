1ca42b35406df41d6402b192fc87e11df1098645
ConstanzaGarcia <o.conii.o@gmail.com>
2018-05-28 20:51:52 -0400

    js arreglado, versión II

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,41 @@
-const menu = function (request) {
-  if (Ceviche < 5) {
-    return 'Tu plato estará listo en 30 minutos';
+  let Ceviche= 0;
+  let Tacos= 1;
+  let EmpanadaChilena = 0;
+  let Hamburguesas= 0;
+
+  switch(menu) {
+    case "Hamburguesas":
+    document.write ("Tu plato estará listo en 15 minutos");
+    break;
+    case "Ceviche":
+    document.write ("En media hora pasalo a buscar");
+    break;
+    case "Tacoss":
+    alert ("NO TENEMOS");
+    break;
+    case "EmpanadaChilena":
+    desafult:
+    alert ("Por el momento se nos agoto, te invitamos a pedir otra cosa");
+    }
+
+
+/* 
+let menu = function (request) { 
+if (Ceviche)
+  {
+    console.log("Tu plato estará listo en 30 minutos");
   }
-  else if (Tacos < 4) {
-    return 'En 15 minutos tu plato estara lista';
+  
+ else if (tacos) {
+    console.log("En 15 minutos tu plato estara lista");
   }
-  else if (EmpanadaChilena < 0 ) {
-  return 'No tenemos en estos momentos pero hay mas opciones para elegir';
+
+  else if (empanada) {
+    console.log("No tenemos en estos momentos pero hay mas opciones para elegir");
+  }
+
+  else (hamburguesa); {
+    console.log("Nos encantan las hamburguesas,pero se nos acabaron");
+  }
 }
- else if (Hambuerguesas < 0) {
-  return "Nos encantan las hamburguesas,pero se nos acabaron"; 
-; } }
+*/
